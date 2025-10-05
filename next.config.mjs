5f76df38fbf6f2b851d041dd8568f8c77a9eb1ec
rutvijmavani <rutvijmavani@gmail.com>
2025-10-05 18:02:26 -0400

    deployment changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    output: 'export', // ensures static export
+    basePath: '/rutvijmavani.github.io',
+    assetPrefix: '/rutvijmavani.github.io/',
+    images: {
+        unoptimized: true, // disables Next.js image optimization (required for static export)
+    }
+};
 export default nextConfig;
