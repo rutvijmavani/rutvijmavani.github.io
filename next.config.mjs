b43781001715a1604605f538ef4d02831b022242
rutvijmavani <rutvijmavani@gmail.com>
2025-10-05 18:30:45 -0400

    path error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // ensures static export
-    basePath: '/rutvijmavani.github.io',
-    assetPrefix: '/rutvijmavani.github.io/',
+    basePath: '',
+    assetPrefix: '',
     images: {
         unoptimized: true, // disables Next.js image optimization (required for static export)
     }
